3bde0b7833a13cbaa994d3825bd6bb4766e3bc89
Wei Shen <tayweishen4@gmail.com>
2023-11-26 00:43:00 +0800

    added portfolio stuff, no prompt enjinear

--- company/views.py
+++ company/views.py
@@ -7,11 +7,7 @@ import json
 
 brt = boto3.client(service_name='bedrock-runtime', region_name="us-east-1")
 
-
-def company(request):
-
-        if request.method == 'POST':
-            user_input = request.POST['user_input']
+def chatbot(user_input):
 
             # Clean and format user input
             clean_input = user_input.strip().lower()
@@ -27,7 +23,7 @@ def company(request):
             "top_p": 0.9,
             })
 
-            modelId = 'anthropic.claude-v1'
+            modelId = 'anthropic.claude-v2'
             accept = 'application/json'
             contentType = 'application/json'
 
@@ -42,9 +38,40 @@ def company(request):
             # escaped_text = cgi.escape(response)
 
             # Render HTML page with generated text
-            context = {'generated_text': response}
-            return render(request, "company/company.html", context)
-        else:
-            return render(request, "company/company.html")
+            return response
+  
+
+def company(request):
+#     prompt = "what to do when suicidal"
+
+#     final_input = "\n\nHuman: {}\n\nAssistant:".format(prompt)
+
+
+#     body = json.dumps({
+#     "prompt": final_input,
+#     "max_tokens_to_sample": 300,
+#     "temperature": 0.1,
+#     "top_p": 0.9,
+#     })
+
+#     modelId = 'anthropic.claude-v1'
+#     accept = 'application/json'
+#     contentType = 'application/json'
+
+#     response = brt.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
+
+#     response_body = json.loads(response.get('body').read())
+
+#     stock_info = response_body.get('completion')
+
+
+
+    if request.method == 'POST':
+        user_input = request.POST['user_input']
+        context = {'chatbot_output': chatbot(user_input), 'stock_info': stock_info}
+        
+        return render(request, "company/company.html", context)  
+    else:
+        return render(request, "company/company.html")
 
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
 
-# Create your views here.
+import boto3
+import json
+
+brt = boto3.client(service_name='bedrock-runtime', region_name="us-east-1")
+
 
 def portfolio(request):
-    return render(request, "portfolio/portfolio.html")
+
+    # Change here
+    recommendations_prompt = "what to do when suicidal"
+
+    recommendations_input = "\n\nHuman: {}\n\nAssistant:".format(recommendations_prompt)
+
+    recommendations_body = json.dumps({
+    "prompt": recommendations_input,
+    "max_tokens_to_sample": 300,
+    "temperature": 0.1,
+    "top_p": 0.9,
+    })
+
+    modelId = 'anthropic.claude-v1'
+    accept = 'application/json'
+    contentType = 'application/json'
+
+    response = brt.invoke_model(body=recommendations_body, modelId=modelId, accept=accept, contentType=contentType)
+
+    response_body = json.loads(response.get('body').read())
+
+    portfolio_recommendations = response_body.get('completion')
+
+    # Change here
+    summary_prompt = "summary of the bee movie"
+
+    summary_input = "\n\nHuman: {}\n\nAssistant:".format(summary_prompt)
+
+    summary_body = json.dumps({
+    "prompt": summary_input,
+    "max_tokens_to_sample": 300,
+    "temperature": 0.1,
+    "top_p": 0.9,
+    })
+
+    response = brt.invoke_model(body=summary_body, modelId=modelId, accept=accept, contentType=contentType)
+
+    response_body = json.loads(response.get('body').read())
+
+    summary = response_body.get('completion')
+
+
+    context = {"recommendations" : portfolio_recommendations, "summary": summary}
+    return render(request, "portfolio/portfolio.html", context)
