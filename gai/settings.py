bf6002e542be85e106d5040363b2f82f57335b5d
yamkeli <yamkeli.work@gmail.com>
2023-11-25 11:36:41 +0800

    added portfolio

--- gai/settings.py
+++ gai/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'company.apps.CompanyConfig',
     'dashboard.apps.DashboardConfig',
+    'portfolio.apps.PortfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
